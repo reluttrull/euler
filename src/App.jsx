8cc48920bf0839ae9e2d30ab17cba0ab3433d37c
Ryan Luttrull <reluttrull@gmail.com>
2025-07-30 16:41:56 -0500

    added #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { euler1 } from './solutions/euler1.jsx';
 import { euler2 } from './solutions/euler2.jsx';
+import { euler3 } from './solutions/euler3.jsx';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const [showing, setShowing] = useState(0);
-  let mySolutions = [{"id": "1"}, {"id": "2"}];
+  let mySolutions = [{"id": "1"}, {"id": "2"}, {"id": "3"}];
   const components = {
-    euler1, euler2
+    euler1, euler2, euler3
   };
 
   const DynamicComponent = ({ componentName }) => {
---
+++ src/solutions/euler3.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+export const euler3 = () => {
+  let numbers = [];
+  let total = 0;
+  const [displayFactors, setDisplayFactors] = useState([]);
+  const [displayHighest, setDisplayHighest] = useState(0);
+
+  const isPrime = (num) => {
+  let maxFactor = Math.floor(Math.sqrt(num));
+    for (let i = 2; i <= maxFactor; i++) {
+      if (num % i == 0) return false;
+    }
+    return true;
+  }
+
+  const getPrimeFactors = (num) => {
+  let maxFactor = Math.floor(Math.sqrt(num));
+    let factors = [];
+    for (let i = 2; i < maxFactor; i++) {
+      if (num % i == 0 && isPrime(i)) factors.push(i);
+    }
+    return factors;
+  }
+
+  useEffect(() => {
+    let factors = getPrimeFactors(600851475143);
+    setDisplayFactors(factors);
+    setDisplayHighest(factors[factors.length-1])
+  }, []);
+
+  return (
+    <>
+      <p>The prime factors of 13195 are 5, 7, 13 and 29.</p>
+      <p>What is the largest prime factor of the number 600851475143?</p>
+      <br />
+      <div className="display-equation">{displayFactors.map(factor => 
+        <span>{factor}{displayFactors.indexOf(factor) + 1 < displayFactors.length ? ", " : ""}</span>
+      )}</div>
+      <h3><span className="spoiler">{displayHighest}</span></h3>
+    </>
+  );
+};
+
+export default euler3;
