c528d196ffc56b7787623704fa38f42e2fc86e20
Ryan Luttrull <reluttrull@gmail.com>
2025-08-29 11:35:06 -0500

    added problem for already solved #9

--- src/solutions/euler9.jsx
+++ src/solutions/euler9.jsx
@@ -32,7 +32,12 @@ export const euler8 = () => {
 
   return (
     <>
-      
+      <p>A Pythagorean triplet is a set of three natural numbers, a &lt; b &lt; c, for which,</p>
+      <p>a^2 + b^2 = c^2.</p>
+      <p>For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.</p>
+      <p>There exists exactly one Pythagorean triplet for which a + b + c = 1000.<br/>
+      Find the product abc.</p>
+
       <br />
       <div className="display-equation">{displayTriples}</div>
       <h3><span className="spoiler">{displayProduct}</span></h3>
